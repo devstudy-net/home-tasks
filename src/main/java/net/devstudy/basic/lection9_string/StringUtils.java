017a278babf017c237080e035ae12f2ed691fc11
Devstudy.Net <devstudy.net@gmail.com>
2016-12-30 17:55:14 +0200

    StringUtils

---
+++ src/main/java/net/devstudy/basic/lection9_string/StringUtils.java
@@ -0,0 +1,67 @@
+package net.devstudy.basic.lection9_string;
+
+/**
+ * 
+ * @author devstudy
+ * @see http://devstudy.net
+ */
+public class StringUtils {
+
+	public static String capitalizeWord(String word) {
+		if (word == null) {
+			return null;
+		} else if (word.isEmpty()) {
+			return word;
+		} else if (word.length() == 1) {
+			return word.toUpperCase();
+		} else {
+			return Character.toUpperCase(word.charAt(0)) + word.substring(1).toLowerCase();
+		}
+	}
+	
+	public static int countMatch(String text, String phrase) {
+		int count = 0;
+		int index = 0;
+		while(true) {
+			index = text.indexOf(phrase, index);
+			if(index == -1) {
+				break;
+			}
+			count++;
+			index += phrase.length();
+		}
+		return count;
+	}
+	
+	public static String join(String[] array, String delimeter) {
+		StringBuilder result = new StringBuilder();
+		if(array != null) {
+			for(int i=0;i<array.length;i++) {
+				result.append(array[i]);
+				if(i != array.length-1) {
+					result.append(delimeter);
+				}
+			}
+		}
+		return result.toString();
+	}
+	
+	public static String replace(String text, String phraseToFind, String phraseToReplace) {
+		return text.replace(phraseToFind, phraseToReplace);
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(capitalizeWord("jaVa"));
+		System.out.println(capitalizeWord("i"));
+		
+		System.out.println(countMatch("Hello world!", "world"));
+		System.out.println(countMatch("Hello world!", "o"));
+		System.out.println(countMatch("ooooo", "o"));
+		System.out.println(countMatch("ooooo", "2"));
+		
+		System.out.println(join(new String[]{"1"},  ","));
+		System.out.println(join(new String[]{"1", "2", "3"},  ","));
+		
+		System.out.println(replace("Hello Java! Java is a language", "Java", "C#"));
+	}
+}
